7b3b94c095b9525303c97ffc8f23940331911b2c
chungeric <rchung.eric@gmail.com>
2018-05-20 00:50:15 +1000

    About page added, styling skills section

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link';
 const Navigation = () => (
   <nav className="navigation">
     <ul>
+      <li><Link to="/about">About</Link></li>
       <li><Link to="/projects">Projects</Link></li>
       <li><Link to="/resume">Resume</Link></li>
     </ul>
---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const AboutPage = () => (
+  <section className="about-page">
+      <h2>About me</h2>
+  </section>
+)
+
+export default AboutPage
