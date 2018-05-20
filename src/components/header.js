152f004b99d52c3d6b663898af61a5ea02a542bf
chungeric <rchung.eric@gmail.com>
2018-05-20 20:21:07 +1000

    Added scroll event header title animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
 
-const Header = ({ siteTitle }) => (
-  <header className="site-header">
-    <div className="title">
-      <h1>
-        <Link to="/" style={{opacity: '0.7'}}><div className="first-name">E<span>ri</span>c</div><span style={{opacity: '1'}}className="last-name">Chung</span></Link>
-      </h1>
-    </div>
-  </header>
-)
+class Header extends Component {
+  constructor(props) {
+    super(props);
 
-export default Header
+    this.state = {
+      riMaxWidth: '9999px',
+      riOpacity: 1,
+      lastNameOpacity: 1
+    };
+
+  }
+
+  componentDidMount() {
+   window.addEventListener('scroll', (e)=>{
+     if (window.scrollY > 0) {
+       this.setState({riMaxWidth: 0, riOpacity: 0, lastNameOpacity: 0})
+     } else {
+       this.setState({riMaxWidth: '9999px', riOpacity: 1, lastNameOpacity: 1})
+     }
+    });
+  }
+
+  render() {
+    return(
+      <header className="site-header">
+        <div className="title">
+          <h1>
+            <Link to="/"><div className="first-name" style={{ opacity: 0.7 }}>E<span style={{ maxWidth: this.state.riMaxWidth, opacity: this.state.riOpacity }}>ri</span>c</div><span style={{opacity: this.state.lastNameOpacity}}className="last-name">Chung</span></Link>
+          </h1>
+        </div>
+      </header>
+    );
+  }
+
+}
+
+export default Header;
