a74f0627194ffc70f7d918ee3a6bcb18e345436d
chungeric <rchung.eric@gmail.com>
2018-06-24 20:57:47 +1000

    About page responsive styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,23 +25,28 @@ class AboutPage extends Component {
     return (
       <section className="about-page">
         <div className="wrapper">
+          { this.state.url &&
+            <img src={this.state.url} alt="Avatar" className="github-avatar"/>
+          }
           <h1 className="page-title">A bit about me</h1>
           <div className="content">
             { this.state.url &&
               <img src={this.state.url} alt="Avatar" className="github-avatar"/>
             }
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia
-            nisl. Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum
-            ornare faucibus. Morbi sagittis non odio ac mollis. Suspendisse tristique ante a eleifend volutpat. Mauris
-            ut sodales leo. Curabitur suscipit sit amet purus facilisis interdum. Duis finibus odio at tempus mollis.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia
-            nisl. Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum
-            ornare faucibus. Morbi sagittis non odio ac mollis. Suspendisse tristique ante a eleifend volutpat. Mauris
-            ut sodales leo. Curabitur suscipit sit amet purus facilisis interdum. Duis finibus odio at tempus mollis.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia
-            nisl. Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum
-            ornare faucibus. Morbi sagittis non odio ac mollis. Suspendisse tristique ante a eleifend volutpat. Mauris
-            ut sodales leo. Curabitur suscipit sit amet purus facilisis interdum. Duis finibus odio at tempus mollis.</p>
+            <div className="text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia
+              nisl. Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum
+              ornare faucibus. Morbi sagittis non odio ac mollis. Suspendisse tristique ante a eleifend volutpat. Mauris
+              ut sodales leo. Curabitur suscipit sit amet purus facilisis interdum. Duis finibus odio at tempus mollis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia
+              nisl. Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum
+              ornare faucibus. Morbi sagittis non odio ac mollis. Suspendisse tristique ante a eleifend volutpat. Mauris
+              ut sodales leo. Curabitur suscipit sit amet purus facilisis interdum. Duis finibus odio at tempus mollis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia
+              nisl. Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum
+              ornare faucibus. Morbi sagittis non odio ac mollis. Suspendisse tristique ante a eleifend volutpat. Mauris
+              ut sodales leo. Curabitur suscipit sit amet purus facilisis interdum. Duis finibus odio at tempus mollis.</p>
+            </div>
           </div>
 
 
