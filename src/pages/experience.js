5cd91417156d86ffb1e97dfdfe8f026d15451712
chungeric <rchung.eric@gmail.com>
2018-07-01 03:54:03 +1000

    Experience page timeline

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -19,6 +19,18 @@ const ExperiencePage = () => (
             </div>
           </div>
         </div>
+        <div className="timeline">
+          <ul className="experiences">
+            <li className="experience">
+              <h2 className="role">Web Developer</h2>
+              <h3 className="company">BirdBrain, Jan 2018 to Present</h3>
+              <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia nisl.
+              Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum ornare faucibus.
+              Morbi sagittis non odio ac mollis. Suspendisse tristique ante a eleifend volutpat. Mauris ut sodales leo.
+              Curabitur suscipit sit amet purus facilisis interdum.</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </section>
