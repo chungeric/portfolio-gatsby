4de3efc6646fc1a163de4d5b00399ed8692ad206
chungeric <rchung.eric@gmail.com>
2018-07-01 18:32:39 +1000

    Styling experience page

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -5,8 +5,10 @@ const ExperiencePage = () => (
   <section className="experience-page">
     <div className="wrapper">
       <h1 className="page-title">Experience</h1>
-      <div className="content">
-        <div className="page-intro">
+    </div>
+    <div className="content">
+      <div className="page-intro">
+        <div className="wrapper">
           <div className="page-intro-left-col">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia nisl.
             Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum ornare faucibus.
@@ -14,20 +16,33 @@ const ExperiencePage = () => (
             Curabitur suscipit sit amet purus facilisis interdum.</p>
           </div>
           <div className="page-intro-right-col">
-            <div className="button" role="button">
-              <a href="#">View resume</a>
+            <div className="resume-link">
+              <a href="#">View resume <i className="fas fa-external-link-alt icon"></i></a>
             </div>
           </div>
         </div>
-        <div className="timeline">
+      </div>
+      <div className="timeline">
+        <div className="wrapper">
           <ul className="experiences">
             <li className="experience">
-              <h2 className="role">Web Developer</h2>
+              <h2 className="role" title="Web Developer">Web Developer</h2>
               <h3 className="company">BirdBrain, Jan 2018 to Present</h3>
-              <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia nisl.
-              Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum ornare faucibus.
-              Morbi sagittis non odio ac mollis. Suspendisse tristique ante a eleifend volutpat. Mauris ut sodales leo.
-              Curabitur suscipit sit amet purus facilisis interdum.</p>
+              <div className="details">
+                <p className="text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac neque egestas, molestie felis id, lacinia nisl.
+                  Integer interdum lorem elit, eget dictum augue pharetra a. Vivamus feugiat diam eget ipsum ornare faucibus.
+                  Morbi sagittis non odio ac mollis. Suspendisse tristique ante a eleifend volutpat. Mauris ut sodales leo.
+                  Curabitur suscipit sit amet purus facilisis interdum.
+                </p>
+                <ul className="skills">
+                  <li className="skill">WordPress</li>
+                  <li className="skill">React</li>
+                  <li className="skill">Magento 2</li>
+                  <li className="skill">GraphQL</li>
+                  <li className="skill">Sass</li>
+                </ul>
+              </div>
             </li>
           </ul>
         </div>
