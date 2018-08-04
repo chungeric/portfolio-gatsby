e65bf099a15baa776d36bf0ddaaa97bf5b409956
chungeric <rchung.eric@gmail.com>
2018-08-05 00:48:08 +1000

    Tweaks + removing commented code

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,30 +4,11 @@ import Navigation from './Navigation';
 import portrait from '../../assets/images/portrait.jpg';
 
 class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isScrolled: false
-    };
-
-  }
-
-  componentDidMount() {
-   // window.addEventListener('scroll', (e)=>{
-   //   if (window.scrollY > 30) {
-   //     this.setState({ isScrolled: true });
-   //   } else {
-   //     this.setState({ isScrolled: false });
-   //   }
-   //  });
-  }
-
   render() {
     return(
       <div id="sidebar">
         <img className="portrait" src={portrait} />
-        <h1 className={"title" + (this.state.isScrolled ? ' scrolled' : '')}>
+        <h1 className="title">
           <Link exact  to="/" activeClassName="active">
             <span className="letters">Eric Chung</span>
           </Link>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,6 +4,7 @@ import Link from 'gatsby-link'
 const BlogPage = () => (
   <section className="blog-page">
     <h1 className="page-title">Blog</h1>
+    <p>Nothing here yet.</p>
   </section>
 )
 
